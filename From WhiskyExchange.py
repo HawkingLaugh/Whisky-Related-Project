d322b9a03535ff7b2f369d913e513b6b527e1a9f
HawkingLaugh <chanhugo@icloud.com>
2020-12-28 20:57:01 +0900

    input on the first

--- From WhiskyExchange.py
+++ From WhiskyExchange.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+name = input("Enter Whisky Brand: ")
 PATH = 'C:\Program Files (x86)\chromedriver.exe'
 # for browser option settings
 chromeOptions = Options()
@@ -13,22 +14,20 @@ chromeOptions.add_argument("--disable-popup-blocking")
 chromeOptions.add_argument("--lang=en")
 driver = webdriver.Chrome(PATH,chrome_options=chromeOptions)
 
-
 try:
-    f = open('whisky_name1.txt', 'r+')
+    f = open('{}.txt'.format(name), 'r+')
 except:
-    f = open('whisky_name1.txt', 'w+')
+    f = open('{}.txt'.format(name), 'w+')
 
-def brandToSearch(name):
-    name = input("Enter Whisky Brand: ")
-    return name
+# def brandToSearch():
+#     name = input("Enter Whisky Brand: ")
+#     return name
 
 def getAllNames(list):
     names = driver.find_elements_by_class_name("name")
     for name in names:
         nameList.append(name.text)
     for name in nameList:
-        # f.write(name)
         if '\n' in name:
             location = name.index('\n')
             f.write(name[0:location] + " : " + name[location + 1:])
@@ -48,10 +47,13 @@ def findElements():
             f.close()
             break
 
+def connectToSite(brandName):
+    driver.get('https://www.thewhiskyexchange.com/')
+    search = driver.find_element_by_id("txtSearchInput")
+    search.send_keys(brandName)
+    search.send_keys(Keys.RETURN)
+
 if __name__ == "__main__":
     nameList = []
-    driver.get('https://www.thewhiskyexchange.com/')
-    print(driver.title)
-    search = driver.find_element_by_id("txtSearchInput")
-    search.send_keys(brandToSearch())
-    search.send_keys(Keys.RETURN)
+    connectToSite(name)
+    findElements()
